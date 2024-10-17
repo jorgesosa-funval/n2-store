e49c688d571db2aed0ad3b1534685f06785cc2e1
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-10-17 15:23:01 -0500

    add tag link

--- components/dropdown.jsx
+++ components/dropdown.jsx
@@ -1,11 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Dropdown() {
     return (
         <div className='border rounded-2xl w-40 py-7 px-4 shadow-md bg-sky-400'>
-            <li className='list-none text-slate-900 hover:underline cursor-pointer'>Profile</li>
-            <li className='list-none text-slate-900 hover:underline cursor-pointer'>Lista de deseos</li>
-            <li className='list-none text-slate-900 hover:underline cursor-pointer'>Logout</li>
+            <li className='list-none text-slate-900 hover:underline cursor-pointer'>
+                <Link href={"#"}>Profile</Link>
+                </li>
+            <li className='list-none text-slate-900 hover:underline cursor-pointer'>
+                <Link href={""}>Lista de deseos</Link>
+                </li>
+            <li className='list-none text-slate-900 hover:underline cursor-pointer'>
+                <Link href={"/api/auth/logout"}>Logout</Link>
+                </li>
         </div>
     )
 }
