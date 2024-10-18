18348d962e96ec8200215cec3ee04a3c7ecc12db
Augusto Cerrillo Molina <augusto_cm@yahoo.com>
2024-10-18 09:47:15 -0500

    Update NavFilterCategory.jsx

--- components/navfiltercategory/NavFilterCategory.jsx
+++ components/navfiltercategory/NavFilterCategory.jsx
@@ -1,19 +1,29 @@
 'use client'
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import axios from "axios";
 
 
-async function fetchData(url) {
-    const rs = await fetch(url)
-    const json = await rs.json()
-    console.log(json)
-    return json
-  }
+export default function NavbarFilterCategory() {
 
-export default async function NavbarFilterCategory() {
 
-const {data : categories} = await axios.get('https://fakestoreapi.com/products/categories',
-    {});
+  const [categories, SetCategories] = useState([]); 
+
+  useEffect(() => {
+    // Usa axios para hacer una solicitud al archivo JSON
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products/categories'); 
+        SetCategories(response.data); 
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+    fetchCategories();
+  }, []); // Ejecuta el 
+
+/*const {data : categories} = await axios.get('https://fakestoreapi.com/products/categories',
+    {});*/
 
 
 
@@ -23,7 +33,7 @@ const {data : categories} = await axios.get('https://fakestoreapi.com/products/c
                 <input type="text" className="rounded-xl h-8 w-[55%] text-sm md:w-[70%] ml-8 border-1 shadow-lg hover:border-cyan-400" placeholder='  Search product by name'/>
                 <div className="w-0 md:w-64"></div>
                 <select name="categories" className="shadow-lg text-sm justify-end md:ml-28 ml-2 h-8 text-slate-600 rounded-md w-[20%] md:w-[30%] mr-10 md:mr-2">
-                   <option value="Todas">Todas</option>
+                <option value="Todas">Todas</option>
                             {  categories &&  categories.map((cat,i) =>
                                 <option key={i} value={cat}>{cat}</option>
                             )}
