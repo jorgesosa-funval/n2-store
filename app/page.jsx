01c6cb1ba1fdb5fcba5cc6f0d2f0a4aea874b8da
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-10-18 10:41:31 -0500

    fix home

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,13 @@
-
-
-import React from 'react'
-
+import React from "react";
 
 export default function Home() {
   return (
-      <>
-       
-      </>
-
     <>
-    <div>
-     
-      <div className='size-44 bg-sky-500'>
+      <div>
+        <div className="size-44 bg-sky-500"></div>
 
+        <div className="size-44 bg-slate-900"></div>
       </div>
-
-      <div className='size-44 bg-slate-900'>
-        
-
-      </div>
-    </div>
     </>
-
-  )
+  );
 }
